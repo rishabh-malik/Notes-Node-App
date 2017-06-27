15fc22b15219ed828cb8bcad5eeb6d9c5a3dfefc
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-27 16:35:46 +0530

    working on the app

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 console.log('Starting App:')
 
+// fs for file input and output
 const fs=require('fs');
+// os for system info
 const os=require('os');
+// added this line so that our notes.js should also start
+const notes=require('./notes.js');
+const _=require('lodash');
 
-var user=
+var command=process.argv[2]
+
+if(command=='add'){
+   console.log('Adding new note'); 
+}else if (command=='list'){
+    console.log('Listing Notes');
+}
---
+++ notes.js
@@ -0,0 +1,8 @@
+console.log('Starting notes.js')
+
+//export used to export functions app.js 
+module.exports.addNote= (a,b) =>{
+var res=a+b;
+return res;
+
+};
